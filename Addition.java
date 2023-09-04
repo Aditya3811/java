bd4f67f14d7f516b14c0168f6e2caa6978cf878d
Aditya3811 <143004503+Aditya3811@users.noreply.github.com>
2023-09-04 21:28:46 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Addition
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first integer");
+        int x = sc.nextInt();
+        System.out.println("enter second integer");
+        int y = sc.nextInt();
+        int z = x-y;
+        System.out.println(x+"-"+y+"="+z);
+        
+    }
+}
