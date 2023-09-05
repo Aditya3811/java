af99da36ef99f9509baf158da9f2c93aa9abdb7c
Aditya3811 <143004503+Aditya3811@users.noreply.github.com>
2023-09-04 20:59:58 -0700

    Create DefaultDemo.java

---
+++ DefaultDemo.java
@@ -0,0 +1,16 @@
+class Sample
+{
+    void display()
+    {
+        System.out.println("hello wolrd");
+    }
+}
+ public class DefaultDemo
+{
+    public static void main(String args [])
+    {
+        Sample s = new Sample();
+        s.display();
+        
+    }
+}
